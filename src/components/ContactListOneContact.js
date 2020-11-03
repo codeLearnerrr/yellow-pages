7186509e37b85fa8161eb3cc8ce06b8575e37317
Jeferson Eiji <jeferson.eiji@icloud.com>
2020-11-03 20:58:26 -0300

    added groups by contact first letter

--- src/components/ContactListOneContact.js
+++ src/components/ContactListOneContact.js
@@ -3,15 +3,13 @@ import '../YellowPages.css';
 export default class ContactListOneContact extends Component {
     render() {
         return (
-            <div>
-                <p style={{ display: 'flex' }} >
-                    {this.props.mapKey} {this.props.lname}
+                <p key={this.props.index} style={{ display: 'flex' }} >
+                    {this.props.lname}
                     {" , "}
                     {this.props.fname}
                     <span id="lineFromNameToNumber"></span>
                     {this.props.contact}
                 </p>
-            </div>
         )
     }
 }
--- src/components/ShowContactsList.js
+++ src/components/ShowContactsList.js
@@ -4,61 +4,74 @@ import '../YellowPages.css';
 
 const mockData = [
     {
-        "A": [
-            {
-                "id": 1,
-                "fname": "Amanda",
-                "lname": "Gonzales",
-                "contact": "(31) 9 9580-2530",
-            },
-            {
-                "id": 2,
-                "fname": "Astrid",
-                "lname": "Guzman",
-                "contact": "(31) 9 9790-2530",
-            },
-            {
-                "id": 3,
-                "fname": "Aurora",
-                "lname": "Muñoz",
-                "contact": "(57) 9 9580-2530",
-            },
-
-        ],
-        "B": [
-            {
-                "id": 4,
-                "fname": "Bernard",
-                "lname": "Rieu",
-                "contact": "(11) 9 4598-2530",
-            },
-            {
-                "id": 5,
-                "fname": "Bruna",
-                "lname": "Argerich",
-                "contact": "(21) 9 9790-7654",
-            },
-            {
-                "id": 6,
-                "fname": "Beatrice",
-                "lname": "Guth",
-                "contact": "(37) 9 7946-2530",
-            },
-        ]
-    }
+        "id": 1,
+        "fname": "Amanda",
+        "lname": "Gonzales",
+        "contact": "(31) 9 9580-2530",
+    },
+    {
+        "id": 2,
+        "fname": "Astrid",
+        "lname": "Guzman",
+        "contact": "(31) 9 9790-2530",
+    },
+    {
+        "id": 3,
+        "fname": "Aurora",
+        "lname": "Muñoz",
+        "contact": "(57) 9 9580-2530",
+    },
+    {
+        "id": 4,
+        "fname": "Bernard",
+        "lname": "Rieu",
+        "contact": "(11) 9 4598-2530",
+    },
+    {
+        "id": 5,
+        "fname": "Bruna",
+        "lname": "Argerich",
+        "contact": "(21) 9 9790-7654",
+    },
+    {
+        "id": 6,
+        "fname": "Zion",
+        "lname": "Guth",
+        "contact": "(37) 9 7946-2530",
+    },
+    {
+        "id": 7,
+        "fname": "Beatrice",
+        "lname": "Guth",
+        "contact": "(37) 9 7946-2530",
+    },
 ];
+
 
 class ShowContactsList extends Component {
     render() {
-        const gotIt = mockData.map((inputMap) => (console.log(inputMap)));
-        return (
-            <div style={{ backgroundColor: 'yellow', width: '80%' }}>
-                {/* <h1>A</h1> */}
-                {/* <TestComponent/> */}
-                {gotIt}
-                {/* {this.printArrayOfAs()} */}
-            </div>
-        )
+        const groupedByLetter = mockData.reduce((groups, contact) => {
+            const letter = contact.fname[0].toUpperCase();
+            const group = groups[letter] || [];
+            group.push(contact);
+            groups[letter] = group;
+            return groups;
+        }, {})
+        return Object.entries(groupedByLetter).map(([letter, contacts], index) => {
+            return (
+                <div key={index} style={{ backgroundColor: 'yellow', width: '80%' }}>
+                    <h1>{letter}</h1>
+                    {contacts.map((inputMap, index) => (
+                        <ContactListOneContact
+                            key={index}
+                            fname={inputMap.fname}
+                            lname={inputMap.lname}
+                            contact={inputMap.contact}
+                        />
+                    ))}
+                </div>
+            );
+        });
     }
 }
 
